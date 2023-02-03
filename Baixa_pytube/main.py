42f18de602812bfd9844c42f7c7ac27ff2dbcf54
kauansr <kakagamerbr3003@gmail.com>
2023-02-03 16:53:09 -0300

    Projetos yt dowloader

---
+++ Baixa_pytube/main.py
@@ -0,0 +1,89 @@
+from pytube import YouTube, Playlist
+import os
+from time import sleep
+
+print('''Digite sua opcao de dowload:
+1 - Download de apenas um video pelo link.
+2 - Download de apenas uma musica pelo link.
+3 - Download de uma playlist musica pelo link.
+4 - Download de uma playlist video pelo link.
+0 - Para sair.
+''')
+print()
+
+
+while True:
+    try:
+        op = int(input("Digite sua opcao: "))
+
+        if op > 4:
+            print('Apenas entre 1 e 4.')
+            sleep(1)
+
+        if op == 0:
+            sleep(1)
+            print('Obrigado por usar.')
+            break
+
+        if op == 1:
+            url = input('digite a url do video: ')
+            yt = YouTube(url)
+
+            print('Titulo:', yt.title)
+            video = yt.streams.get_highest_resolution()
+            video.download(output_path="C:/Users/ADM/Downloads")
+            print('Download concluido!')
+
+        elif op == 2:
+            
+            url = input(str('Digite sua(s) urls: '))
+
+            
+           
+            
+            ys = YouTube(url)
+            
+            print('Titulo:', ys.title, 'Baixando...')
+            musica = ys.streams.get_audio_only()
+            baixa = musica.download(
+                output_path="C:/Users/ADM/Music")
+            base, ext = os.path.splitext(baixa)
+            novo_arquivo = base + '.mp3'
+            os.rename(baixa, novo_arquivo)
+        
+            print(ys.title, 'Download completo!')
+
+            
+
+        elif op == 3:
+            
+            url = input(str('Digite sua url da playlist de musica: '))
+            yt = Playlist(url)
+
+
+            for musicas in yt.video_urls:
+                ys = YouTube(musicas)
+                print('Titulo:', ys.title, 'Baixando...')
+                musica = ys.streams.get_audio_only()
+                baixa = musica.download(
+                    output_path="C:/Users/ADM/Music")
+                base, ext = os.path.splitext(baixa)
+                novo_arquivo = base + '.mp3'
+                os.rename(baixa, novo_arquivo)
+        
+
+                print(ys.title, 'Download completo!')
+
+        elif op == 4:
+            url = input('Digite sua url da playlist de videos: ')
+            yt = Playlist(url)
+
+            for videos in yt.video_urls:
+                ys = YouTube(videos)
+                print('Titulo:', ys.title, 'Baixando...')
+                video = ys.streams.get_highest_resolution()
+                baixa = video.download(
+                    output_path="C:/Users/ADM/Downloads")
+                print(ys.title, 'Download completo!')
+    except ValueError:
+        print('Apenas entre 1 e 4.')
